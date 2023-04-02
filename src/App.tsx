a3bd80568b5c223185f48ea532164605d7a1a110
Eduard <blademan82@gmail.com>
2023-04-02 22:09:44 +0300

    Build responsive layouts with Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 function App() {
 	return (
-		<div className='App'>
-			<Button colorScheme='facebook'>Button</Button>
-		</div>
+		<Grid
+			templateAreas={{
+				base: `"nav" "aside" "main"`,
+				md: `"nav" "aside" "main"`, // 768px	48rem
+				lg: `"nav nav" "aside main"`, // 992px	62rem
+			}}
+		>
+			<GridItem gridArea='nav' h='20' bg={'blue.300'}>
+				Nav
+			</GridItem>
+			<Show above='lg'>
+				<GridItem gridArea='aside'>Aside</GridItem>
+			</Show>
+			<GridItem bg='gold' h='max' gridArea='main'>
+				Main
+			</GridItem>
+		</Grid>
 	)
 }
 
