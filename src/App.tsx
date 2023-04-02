ce7e0865b86f1d55d380f22a7df88ec4c7a9354d
Eduard <blademan82@gmail.com>
2023-04-02 21:22:26 +0300

    Install Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@chakra-ui/react'
+function App() {
+	return (
+		<div className='App'>
+			<Button colorScheme='facebook'>Button</Button>
+		</div>
+	)
+}
+
+export default App
